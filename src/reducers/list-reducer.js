130fd385753eb74c31eee5be77fa8a5536a0784c
Aniruddh Gopale <anigopale@gmail.com>
2017-10-14 15:25:50 +0530

    list reducer updated, insertElement action appends elements to global state

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,4 +1,14 @@
+import _ from 'lodash';
+
 export default function(state=[],action){
   console.log("payload reached:",action.payload);
+  const temp = [...state, action.payload]
+
+  console.log(temp);
+   switch (action.type) {
+     case 'INSERT_ELEMENT':
+     return [...state, action.payload]
+
+   }
   return state;
 }
